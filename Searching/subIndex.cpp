2aaa9a85348ab66c84394fbc68a168e9704e6907
Arjun <arj.kashyap123@gmail.com>
2019-10-15 00:08:39 +0530

    fix

--- Searching/subIndex.cpp
+++ Searching/subIndex.cpp
@@ -1,28 +1,29 @@
 // Find the subarray with the sum equal to given integer
 // using two pointer approach
-
 #include<bits/stdc++.h>
 using namespace std;
 
 void subarraySum(int arr[], int n, int s){
-   int sum = 0;
-   int l = 0, h = n-1;
-   for(int i = 0; i < n; i++)
-       sum += arr[i];
-   while(l <= h){
-      cout << sum << endl;
-      if ( sum == s ){
-          cout << l+1 << " " << h+1 << endl;
-          return;
-      }
-      else if( sum < s ){
-        sum -= arr[l];
-        l++;
-      }
-      else{
-          sum -= arr[h];
-          h--;
-      }
+   int sum = arr[0];
+   int l_index = 0, r_index = 1;
+  
+   while(r_index <= n && l_index < n){
+       
+       if( sum == s ){
+            cout << l_index + 1 << " " << r_index << endl;
+            return;
+       }
+
+       else if ( sum > s ){
+        sum -= arr[l_index];
+        l_index++;
+       }
+
+       else{
+        sum += arr[r_index];
+        r_index++;
+       }
+       // cout << r_index << endl;        
    }
    cout << -1 << endl;
 }
