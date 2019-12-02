dc4b89a4de942e54bbafb0279f5b572477338bbc
Arjun <arj.kashyap123@gmail.com>
2019-12-02 22:27:44 +0530

    n/2 ex

--- Searching/occNK.cpp
+++ Searching/occNK.cpp
@@ -3,7 +3,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void freqSort()
+void freqSort(int arr[], int n)
+{
+    int i = 0;
+    while( i < n ){
+        if( arr[i] <= 0 ){
+            i++;
+            continue;
+        }
+
+        int elementIndex = arr[i] - 1;
+        if( arr[elementIndex] > 0 ){
+            arr[i] = arr[elementIndex];
+            arr[elementIndex] = -1;
+        }
+        else{
+            arr[elementIndex]--;
+            arr[i] = 0;
+            i++;
+        }
+    }
+}
 
 int countOccurence(int arr[], int n, int k)
 {
@@ -23,7 +43,7 @@ int main(){
         int n, k;
         scanf("%d", &n);
         int arr[n];
-        for( int i = 0; i < n; i++ ) scanf("%d", &n);
+        for( int i = 0; i < n; i++ ) scanf("%d", &arr[i]);
         scanf("%d", &k);
         printf("%d\n", countOccurence(arr, n, k));
 
