d3382ffb2a8db8352023681a51ba0ccd0746c32c
Arjun <arj.kashyap123@gmail.com>
2019-12-13 23:50:31 +0530

    Quick sort using horse partition logic

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,22 @@
+// Quick sort algo
+// Using Hoarse algorithm for partition
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Display(int *a, int n){
+    for( int i = 0; i < n; i++ )
+        printf("%d ", a[i]);
+    printf("\n");
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for( int i = 0; i < n; i++ )
+        scanf("%d", &arr[i]);
+    
+
+    return 0;
+}
