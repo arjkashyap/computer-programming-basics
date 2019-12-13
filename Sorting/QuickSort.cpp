8a0b06a467fecb928709b93e2753cc3c52c63751
Arjun <arj.kashyap123@gmail.com>
2019-12-14 00:33:47 +0530

    Quick sort bug fix

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -10,13 +10,40 @@ void Display(int *a, int n){
     printf("\n");
 }
 
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int partitionIndex(int *a, int start, int end){
+    int pivot = a[end];                 // Pivot element
+    int partitionIndex = start;            
+    for(int i = start; i < end; i++){
+        if( a[i] <= pivot ){
+            swap(&a[i], &a[partitionIndex]);
+            partitionIndex++;
+        }
+    }
+    swap(&a[partitionIndex], &a[end]);
+    return partitionIndex;
+}
+
+void QuickSort(int *a, int start, int end){
+    if( start < end ){
+        int pIndex = partitionIndex(a, start, end);
+        QuickSort(a, start, pIndex-1);
+        QuickSort(a, pIndex + 1, end);
+    }
+}
+
 int main(){
     int n;
     scanf("%d", &n);
     int arr[n];
     for( int i = 0; i < n; i++ )
         scanf("%d", &arr[i]);
-    
-
-    return 0;
+   QuickSort(arr, 0, n -1); 
+   Display(arr, n);
+   return 0;
 }
