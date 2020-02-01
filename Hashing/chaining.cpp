c442af123b38913bb8bd98ae917339d13f8bd1a2
Arjun <arj.kashyap123@gmail.com>
2020-02-01 23:13:05 +0530

    chaining

---
+++ Hashing/chaining.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#include<list>
+using namespace std;
+
+class MyHash{
+    private:
+        int BUCKET;                 // Length of hash table
+        list<int> *table;           // Hash table
+  
+    public:
+        MyHash(int b)
+        {
+            BUCKET = b;
+            table = new list<int>[BUCKET];
+        }
+
+        int hash(int);          // Returns the key from hash table
+        bool search(int);        // Returns true if element is found
+        void insert(int);       // Insert element in hash table
+        void remove(int);           // Delete Element from table
+
+};
+
+int MyHash::hash(int key)
+{
+    return (key%BUCKET);
+}
+
+bool MyHash::search(int s)
+{
+    int index = hash(s);
+    for(auto x : table[index]){
+        if( x == s )
+            return true;
+    }
+    return false;
+}
+
+void MyHash::insert(int a)
+{
+    int index = hash(a);
+    table[index].push_back(a);
+}
+
+void MyHash::remove(int a)
+{
+    int index = hash(a);
+    table[index].remove(a);
+}
+
+int main()
+{
+    MyHash ht(7);
+    ht.insert(12);
+    ht.insert(2);
+    ht.insert(13);
+    ht.insert(7);
+    ht.insert(1);
+    ht.insert(14);
+    ht.insert(9);
+    ht.insert(12);
+    
+    if(ht.search(12))
+        cout << "12 found\n";
+    else
+        cout << "Not found\n";    
+    
+    if(ht.search(9))
+        cout << "9 found\n";
+    else
+        cout << "Not found\n";    
+
+    ht.remove(12);
+    ht.remove(9);
+
+    if(ht.search(12))
+        cout << "12 found\n";
+    else
+        cout << "Not found\n";    
+    
+    if(ht.search(9))
+        cout << "9 found\n";
+    else
+        cout << "Not found\n";    
+    return 0;
+}
+
+
