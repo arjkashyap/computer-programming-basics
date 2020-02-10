4bd091072437f3f2b2b10f8b9146dbe662c1fdac
Arjun <arj.kashyap123@gmail.com>
2020-02-10 22:12:12 +0530

    intersections using hashing

--- Hashing/countIntersection.cpp
+++ Hashing/countIntersection.cpp
@@ -1,33 +1,33 @@
-// Count the number of intersections between two array
+// Counting the number of intersections in an array usning hashing
 #include<bits/stdc++.h>
 #include<unordered_set>
+
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
     int t;
     cin >> t;
     while(t--){
-        unordered_set<int> us;
+        unordered_set<int> set;
         int n1, n2;
-        cin >> n1;
-        for(int i = 0; i < n1; i++){
+        cin >> n1 >> n2;
+        for( int i = 0; i < n1; i++ ){
             int a;
             cin >> a;
-            us.insert(a);
+            set.insert(a);
         }
-        cin >> n2;
-        int a2[n2];
-        for(int i = 0; i < n2; i++) cin >> a2[i];
-        int res = 0;
-        for(int i = 0; i < n2; i++){
-            if( us.find(a2[i]) != us.end() ){
-                res++;
-                us.erase(a2[i]);
+        int count = 0;
+        for( int i = 0; i < n2; i++ ){
+            int s;
+            cin >> s;
+            if(set.find(s) != set.end()){
+                count++;
+                set.erase(s);
             }
         }
-        cout << res << "\n";
+        cout << count << "\n";
     }
-
     return 0;
 }
