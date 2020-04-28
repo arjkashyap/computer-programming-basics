900346c6ffecd27062e0afebb1966ffc263ea9ee
Arjun <arj.kashyap123@gmail.com>
2020-04-28 13:18:41 +0530

    Doubly ll insertion problem

---
+++ LinkedLists/doublyLinkedListInsertion.cpp
@@ -0,0 +1,93 @@
+// Program to insert an element after a specific posiiton in a doubly linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *n = new Node(x);
+    
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
+}
+
+void printList(Node *head)
+{
+  Node *temp=head;
+  if (temp != NULL) {
+ 
+  while (temp->next!=NULL)
+    temp=temp->next;
+  while (temp->prev!=NULL)
+   temp = temp->prev;
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL; 
+  Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+     if(root==NULL) root = head;
+  }     
+  head = root;
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+
+/* Function to insert into a Doubly Linked List */
+void addNode(Node *head, int pos, int data)
+{
+   // Your code here
+   Node* newNode = new Node(data);
+   
+   int index = 0;
+   Node* ptr = head;
+   while(index < pos){
+       ptr = ptr -> next;
+       index++;
+   }
+   newNode -> next = ptr -> next;
+   newNode -> prev = ptr;
+   ptr -> next = newNode;
+   
+
+}
