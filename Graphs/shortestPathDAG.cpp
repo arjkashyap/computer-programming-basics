585a8d512c15943b0f705965ea75914e56460c35
Arjun <arj.kashyap123@gmail.com>
2020-05-27 16:57:55 +0530

    topological sorting in dfs

---
+++ Graphs/shortestPathDAG.cpp
@@ -0,0 +1,51 @@
+// Shortest path in a directed acyclic graph to every other vertex
+//
+// Implementation of topological sorting using dfs
+// Detect cycle in a cyclic directed graph 
+
+#include<bits/stdc++.h>
+
+typedef std::vector<int> g;
+
+void addEdge(g *adj, int u, int v)
+{
+    adj[u].push_back(v);
+}
+
+void dfsSort(g *adj, int s, bool *vis, std::stack<int> &st)
+{
+    vis[s] = true;
+    for(auto v : adj[s])
+        if(vis[v] == false)
+            dfsSort(adj, v, vis, st);
+    
+    st.push(s);
+}
+
+void topoSort(g *adj, int V)
+{
+    bool vis[V];
+    std::stack<int> s;
+    for(int i = 0; i < V; i++)
+        vis[i] = false;
+    for(int i = 0; i < V; i++)
+        if(vis[i] == false)
+            dfsSort(adj, i,  vis, s);
+    while(s.empty() == false){
+        std::cout << s.top() << " ";
+        s.pop();
+    }
+}
+
+int main()
+{
+    int V = 6;
+    g adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    addEdge(adj, 2, 4);
+    topoSort(adj, V);
+    return 0;
+}
--- Graphs/topologicalSortingBFS.cpp
+++ Graphs/topologicalSortingBFS.cpp
@@ -80,3 +80,51 @@ int main()
 
 
 
+// Detect cycle in a cyclic directed graph 
+// using Kahns algorithm
+
+#include<bits/stdc++.h>
+
+typedef std::vector<int> g;
+
+void addEdge(g *adj, int u, int v)
+{
+    adj[u].push_back(v);
+}
+
+bool isCycle(adj, V)
+{
+    int ind[V] = {0};
+    for(int i = 0; i < V; i++){
+        for(auto u : adj[i])
+            ind[u]++;
+    }
+    int count = 0;
+    std::queue<int> q;
+    for(int i = 0; i < V; i++)
+        if(ind[i] == 0)
+            q.push(i);
+    while(q.empty() == false){
+       int curr = q.front();
+       q.pop();
+       for(auto u : adj[curr]){
+        ind[u]--;
+        if(ind[u] == 0)
+            q.push(u);
+       }
+       count++;
+    }
+    return count != V;
+}
+
+int main()
+{
+    int V = 5;
+    g adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    addEdge(adj, 2, 4);
+    return 0;
+}
