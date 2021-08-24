2a2a639397074022ab9894c9c6ffed9b1220f7ba
Arjun Kashyap <arjunk@cloudthing.com>
2021-08-24 17:15:56 +0530

    longest increasing subsequence dp solution

--- Dynamic Programming/longestIncreasingSubseq-dp.cpp
+++ Dynamic Programming/longestIncreasingSubseq-dp.cpp
@@ -1,76 +1,75 @@
-/*
-	LONGEST INCREASING SUBSEQ:
-	                      n        lastSmallestElement=4,    count=4
-	i/p: 3, 4, 2, 8, 10
-	o/P; 4
-	
-	i/P; 4, 10, 6, 5, 8, 11, 2, 20
-	o/p: 5
-	
-	i/P: 10, 20, 30
-	o/P: 3
-	
-	i/P: 30, 20, 10
-	o/P: 1
-	
-	
-*/
-
-#include<iostream>
-#include<vector>
-
+// { Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
 
-int lis(int arr[], int n, int count, int lastSmallestElement, vector<int>& dp)
+ // } Driver Code Ends
+
+
+class Solution
 {
-	int result; 			
-	if(dp[n] != -1){
-		return dp[n];
-	}
-	else{
-		if(n < 1)
-			return count;
+    public:
+    
+    /*
+        dp[i] = dp[j -> j is index of closest smaller element from i] + 1
+                           i
+      arr      5  8  3  7  9  1
+      dp     [ 1  2  1  2  3  1 ]
+                     i
+        i/p: 5 8 3 7 9 1
+        o/p: 3
+    */
+    
+    int getMax(vector<int> v)
+    {
+        int res = INT_MIN;
+        for(auto x : v){
+            if(x > res)
+                res = x;
+        }
+        return res;
+    }
+    
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+        int res = -1;
+        vector<int> dp(n, -1);
+        for(int i = 0; i < n; i++){
+            int lisSize = 0; // length of longest inc subseq uptill i
+            int j = i-1;
+            while(j >= 0){
+                if(a[j] < a[i]){
+                    lisSize = max(lisSize, dp[j]);
+                }
+                j--;
+            }
+            dp[i] = 1 + lisSize;
+        }
+        res = getMax(dp);
+        return res;
+    }
+};
 
-		int resultExcluding;   // result we get if we include the current element
-		int resultIncluding;    // result we get if we exclude it
-		
-		resultExcluding = lis(arr, n-1, count, lastSmallestElement, dp);
-		// include arr[n-1]th element
-		if(arr[n-1] < lastSmallestElement){
-			resultIncluding = lis(arr, n-1, count+1, arr[n-1], dp);
-		}
-		
-		result = max(resultIncluding, resultExcluding);
-		dp[n] = result;
-		return result;
-	}
-}
 
 
-int longestIncreasingSubseq(int* a, int n)
-{
-	if(n==0 || n==1) return n;
-	
-	int res = 1;
-	vector<int> dp(n+1, -1);
-	
-	res = lis(a, n-1, res, a[n-1], dp);
-	return res;
-}
-
+// { Driver Code Starts.
 int main()
 {
-	int t;
-	cin >> t;
-	while(t--){
-		int n;
-		cin >> n;
-		int arr[n];
-		for(int i = 0; i < n; i++) cin >> arr[i];
-		
-		int res = longestIncreasingSubseq(arr, n);
-		
-		cout << res << endl;
-	}
-	return 0;
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
 }
+  // } Driver Code Ends
